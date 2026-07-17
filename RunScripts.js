1388bc00fb11b013957c8423bd87fb62d70cd2b0
lxp <lixupeng@jasolar.com>
2026-07-17 17:12:11 +0800

    RunScripts.js 简化，因为从github改为代理，应该没有网络问题，无需单独下载万象

--- RunScripts.js
+++ RunScripts.js
@@ -1,45 +1,15 @@
 const path = require('path')
-/** @type {string} */
-const PROJECT_ROOT = __dirname;
 /**
- * @type {(type?: string) => Promise<{totalCount: number, successCount: number, skipCount: number, hasWarn: boolean}>}
+ * @type {() => Promise<{totalCount: number, successCount: number, skipCount: number, hasWarn: boolean}>}
  */
-const updateFiles = require(path.join(PROJECT_ROOT, 'scripts', 'download'));
+const updateFiles = require(path.join(__dirname, 'scripts', 'download'));
 /** @type {() => void} */
-const transform = require(path.join(PROJECT_ROOT, 'scripts', 'transform'));
+const transform = require(path.join(__dirname, 'scripts', 'transform'));
 
-const args = process.argv.slice(2);
 (async () => {
-    let dlResult = null;
     try {
-        if (args.length > 0) {
-            switch (args[0]) {
-                case 'wanxiang':
-                    dlResult = await updateFiles('wanxiang');
-                    break;
-                case 'All':
-                    dlResult = await updateFiles('All');
-                    break;
-                default:
-                    process.exit(1);
-            }
-        } else {
-            dlResult = await updateFiles();
-        }
+        await updateFiles();
         await transform();
-
-        if (dlResult && dlResult.hasWarn) {
-            console.log(`
-░▒▓████████▓▒░▒▓███████▓▒░░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓███████▓▒░  
-░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓██████▓▒░ ░▒▓███████▓▒░░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░  
-░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░ 
-`);
-            process.exit(1);
-        }
         console.log('\n✓ All done');
     } catch (error) {
         console.error('执行过程中出现错误:', error);
