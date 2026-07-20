ba8444478f1de15d528660ccfa0c6b46a1477974
lxp <lixupeng@jasolar.com>
2026-07-20 17:46:49 +0800

    DPY 像 T93 那样，可以改声调（把7 8 9 0 00改为67890就是为了这个）

--- lua/imr/DPY_processor.lua
+++ lua/imr/DPY_processor.lua
@@ -8,7 +8,25 @@ local Processor = {
             and (context:is_composing() or context:has_menu())
         then
             local key_repr = key:repr()
-            -- 辅码模式处理，允许tab后输入/删除辅码
+            -- 声调快速修改：6=一声 7=二声 8=三声 9=四声 0=轻声
+            if key_repr:match('^[67890]$') then
+                local text = context.input
+                if not text:match(';') then
+                    local last = text:sub(-1)
+                    if last:match('^[67890]$') then
+                        if last == key_repr then
+                            context.input = text:sub(1, -2)
+                        else
+                            context.input = text:sub(1, -2) .. key_repr
+                        end
+                        return 1
+                    elseif text:match('[a-z][a-z]$') then
+                        context.input = text .. key_repr
+                        return 1
+                    end
+                end
+            end
+            -- 辅码模式处理，允许输入/删除辅码
             if context.input:match(';') then
                 if key_repr:match('^[a-z]$') then
                     local caret_pos = context.caret_pos == #context.input and #context.input + 1 or context.caret_pos
